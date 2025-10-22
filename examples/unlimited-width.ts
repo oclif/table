f29036fd8478dcb132d00ed664f750cbcfc61d0e
Mike Donnalley <mdonnalley@salesforce.com>
2025-10-22 09:24:43 -0600

    feat: support maxWidth none

---
+++ examples/unlimited-width.ts
@@ -0,0 +1,37 @@
+import {printTable} from '../src/index.js'
+
+const description =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'.repeat(
+    5,
+  )
+
+const data = [
+  {
+    age: 20,
+    description,
+    id: '36329',
+    name: 'Alice',
+  },
+  {
+    age: 21,
+    description,
+    id: '49032',
+    name: 'Bob',
+  },
+  {
+    age: 22,
+    description,
+    id: '51786',
+    name: 'Charlie',
+  },
+]
+printTable({
+  borderStyle: 'none',
+  columns: ['id', 'name', 'age', 'description'],
+  data,
+  headerOptions: {
+    formatter: 'capitalCase',
+  },
+  maxWidth: 'none',
+  titleOptions: {bold: true},
+})
